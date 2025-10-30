18646417c63f51c7d9907ba0eda5dd91184a1ebb
mohammaddaud88 <daudansarikld@gmail.com>
2025-10-31 01:36:56 +0530

    add functionality is created

--- todo_main/urls.py
+++ todo_main/urls.py
@@ -17,8 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home)
+    path('',views.home, name='home'),
+
+    # Todo 
+    path('todo/',include('todo.urls'))
 ]
--- todo_main/views.py
+++ todo_main/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+from todo.models import Task
 
 def home(request):
-    return render(request,'home.html')
+    tasks = Task.objects.filter(is_completed=False).order_by('updated_at')
+    # print(tasks)
+    context = {'tasks':tasks}
+    return render(request,'home.html',context)
